890d9b08a448d5de1f156999ddc2ae1fb3dcdf2c
lambertjacob <75557870+lambertjacob@users.noreply.github.com>
2024-10-03 18:04:57 -0400

    dataset with scaling and return tensor

--- lab_2/snoutnet_dataset.py
+++ lab_2/snoutnet_dataset.py
@@ -1,7 +1,9 @@
-import os
+import os, re
 import pandas as pd
 from torchvision.io import read_image
 from torch.utils.data import Dataset
+from PIL import Image
+import torch
 
 class SnoutNetDataset(Dataset):
     def __init__(self, annotations_file, imgs_dir, transform=None, target_transform=None):
@@ -16,9 +18,29 @@ class SnoutNetDataset(Dataset):
     def __getitem__(self, idx):
         #annotations csv is in form <img name>,"<location>"
         img_path = os.path.join(self.imgs_dir, self.annotations.iloc[idx, 0])
-        img = read_image(img_path)
-        label = self.annotations.iloc[idx,1]
+        img_nonformat = Image.open(img_path)
+        img = img_nonformat.convert('RGB').resize((227,227))
+        
+        #need to get coordinates
+        file_coords = self.annotations.iloc[idx,1]
+        coords = re.findall(r"\((\d+)\,\s(\d+)\)", file_coords)[0]
+        coords = [int(x) for x in coords]
 
+        scale_x = 227 / img_nonformat.size[0]
+        scale_y = 227 / img_nonformat.size[1]
+
+
+        x = int(coords[0] * scale_x)
+        y = int(coords[1] * scale_y)
+        
+        label = torch.tensor([x,y])
+
+        # print("File Size: ")
+        # print(img_nonformat.size)
+        # print("Scaled X: " + str(scale_x) + " Scaled Y: " + str(scale_y))
+        # print("New X: " + str(x) + " New Y: " + str(y))
+        # print("Ori X: " + str(coords[0]) + " Ori Y: " + str(coords[1]))
+        
         if self.transform:
             img = self.transform(img)
         
